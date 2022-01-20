d8de8393675fb949b2aaccff695e68c6c4314de7
Leandro <leandro83g.development@gmail.com>
2022-01-20 12:49:45 +0900

    Relationships&HyperlinkedAPIs/create-endopoint-for-the-root

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,10 @@
-from email.policy import default
-from pyexpat import model
 from rest_framework import serializers
 from snippets.models import Snippet
 from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,17 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users', reverse('user-list',request=request, format=format),
+        'snippets', reverse('snippets-list',request=request, format=format),
+    })
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
