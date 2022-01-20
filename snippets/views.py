d38a658ca2fe80f6646595e21b3facb056e56356
Leandro <leandro83g.development@gmail.com>
2022-01-20 12:56:13 +0900

    Relationships&HyperlinkedAPIs/highglighted-snipets-end-point
    GenricsAPIViewを使ってSnippetHighgtっていうViewつまりendpointを作った
    注目すべきはrenderes.StaticHTMLRenderer
    これは pre-rendered HTML だからtamplateを使わずにあらかじめrest_frameworkが提供しているHTMLを使うってことかな？
    
    api_rootのpath
    snippetHighlightのpath
    それぞれ設定

--- snippets/views.py
+++ snippets/views.py
@@ -2,7 +2,7 @@ from operator import imod
 from snippets.models import Snippet
 from django.contrib.auth.models import User
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -13,7 +13,6 @@ def api_root(request, format=None):
         'users', reverse('user-list',request=request, format=format),
         'snippets', reverse('snippets-list',request=request, format=format),
     })
-
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -28,6 +27,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
